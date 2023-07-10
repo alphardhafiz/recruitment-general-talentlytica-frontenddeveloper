3eba04729b43e9b25f7373d87086a70e635834fb
alphardhafiz <bucialphard@gmail.com>
2023-07-10 07:09:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [penilaian, setPenilaian] = useState({
+    aspek_penilaian_1: {},
+    aspek_penilaian_2: {},
+    aspek_penilaian_3: {},
+    aspek_penilaian_4: {},
+  });
+  const [jsonOutput, setJsonOutput] = useState("");
+
+  const handleInputChange = (aspek, mahasiswa, value) => {
+    setPenilaian((prevState) => ({
+      ...prevState,
+      [aspek]: {
+        ...prevState[aspek],
+        [mahasiswa]: value,
+      },
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const jsonData = JSON.stringify(penilaian, null, 2);
+    setJsonOutput(jsonData);
+  };
+
+  const renderForm = () => {
+    const aspekPenilaian = [
+      "aspek_penilaian_1",
+      "aspek_penilaian_2",
+      "aspek_penilaian_3",
+      "aspek_penilaian_4",
+    ];
+
+    const mahasiswa = [
+      "mahasiswa_1",
+      "mahasiswa_2",
+      "mahasiswa_3",
+      "mahasiswa_4",
+      "mahasiswa_5",
+      "mahasiswa_6",
+      "mahasiswa_7",
+      "mahasiswa_8",
+      "mahasiswa_9",
+      "mahasiswa_10",
+    ];
+
+    return (
+      <form onSubmit={handleSubmit} className="form-penilaian">
+        {aspekPenilaian.map((aspek) => (
+          <div className="aspek-penilaian" key={aspek}>
+            <h3>{aspek}</h3>
+            {mahasiswa.map((mhs) => (
+              <div key={mhs}>
+                <label htmlFor={mhs}>{mhs}:</label>
+                <input
+                  type="number"
+                  id={mhs}
+                  name={mhs}
+                  min="1"
+                  max="10"
+                  onChange={(e) =>
+                    handleInputChange(aspek, mhs, e.target.value)
+                  }
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Penilaian Mahasiswa</h1>
+      {renderForm()}
+      <h2>JSON Output:</h2>
+      <pre>{jsonOutput}</pre>
     </div>
   );
 }
